bdf5213577b482aeec54d9d7eba0bb73634e60d4
Vinicius de Santana <vinicius.vts@gmail.com>
2025-11-04 11:35:49 -0300

    test: enhance UploadFacadeTest with additional validation cases for file types and sizes

--- tests/Feature/UploadFacadeTest.php
+++ tests/Feature/UploadFacadeTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Zigtecnologia\Upload\Facades\Upload;
-use Zigtecnologia\Upload\Enums\UploadErrorEnum;
 
 beforeEach(function () {
     $this->tmpDir = sys_get_temp_dir() . '/filevalidator_test';
@@ -15,7 +14,71 @@ afterEach(function () {
     @rmdir($this->tmpDir);
 });
 
-it('fails validation if an exception is thrown', function () {
+it('fails if the file has incorrect mimetype', function () {
+    $tmpFile = $this->tmpDir . '/large.txt';
+    file_put_contents($tmpFile, str_repeat('a', 2 * 1024)); // 2 KB
+    $_FILES['arquivo'] = [
+        'tmp_name' => $tmpFile,
+        'name' => 'large.txt',
+        'size' => 2 * 1024,
+    ];
+    $path = Upload::make()
+        ->mimetype(['text/plain'])
+        ->folder('uploads')
+        ->upload($_FILES['arquivo']);
+    expect($path)->toBeString();
+    expect(file_exists($path))->toBeTrue();
+});
+
+it('throws exception when file has incorrect mimetype', function () {
+    $tmpFile = $this->tmpDir . '/document.txt';
+    file_put_contents($tmpFile, str_repeat('a', 2 * 1024)); // 2 KB
+    $_FILES['arquivo'] = [
+        'tmp_name' => $tmpFile,
+        'name' => 'document.txt',
+        'size' => 2 * 1024,
+    ];
+    
+    expect(fn() => Upload::make()
+        ->mimetype(['image/jpeg', 'image/png'])
+        ->folder('uploads')
+        ->upload($_FILES['arquivo']))
+        ->toThrow(Exception::class);
+});
+
+it('fails if the file has incorrect extension', function () {
+    $tmpFile = $this->tmpDir . '/large.txt';
+    file_put_contents($tmpFile, str_repeat('a', 2 * 1024)); // 2 KB
+    $_FILES['arquivo'] = [
+        'tmp_name' => $tmpFile,
+        'name' => 'large.txt',
+        'size' => 2 * 1024,
+    ];
+    $path = Upload::make()
+        ->extensions(['txt'])
+        ->folder('uploads')
+        ->upload($_FILES['arquivo']);
+    expect($path)->toBeString();
+    expect(file_exists($path))->toBeTrue();
+});
+
+it('throws exception when file has incorrect extension', function () {
+    $tmpFile = $this->tmpDir . '/document.txt';
+    file_put_contents($tmpFile, str_repeat('a', 2 * 1024)); // 2 KB
+    $_FILES['arquivo'] = [
+        'tmp_name' => $tmpFile,
+        'name' => 'document.txt',
+        'size' => 2 * 1024,
+    ];
+    
+    expect(fn() => Upload::make()
+        ->extensions(['jpg', 'png', 'pdf'])
+        ->folder('uploads')
+        ->upload($_FILES['arquivo']))
+        ->toThrow(Exception::class);
+});
+
+it('fails if the file is bigger than test', function () {
     $tmpFile = $this->tmpDir . '/large.txt';
     file_put_contents($tmpFile, str_repeat('a', 2 * 1024 * 1024)); // 2 MB
     $_FILES['arquivo'] = [
@@ -24,10 +87,25 @@ it('fails validation if an exception is thrown', function () {
         'size' => 2 * 1024 * 1024,
     ];
     $path = Upload::make()
-        ->extensions(['txt','jpg','jpeg','png','plain'])
-        ->maxSize(3)
+        ->maxSize(3) // 3 MB
         ->folder('uploads')
         ->upload($_FILES['arquivo']);
     expect($path)->toBeString();
     expect(file_exists($path))->toBeTrue();
+});
+
+it('throws exception when file has incorrect size', function () {
+    $tmpFile = $this->tmpDir . '/document.txt';
+    file_put_contents($tmpFile, str_repeat('a', 2 * 1024 * 1024)); // 2 MB
+    $_FILES['arquivo'] = [
+        'tmp_name' => $tmpFile,
+        'name' => 'document.txt',
+        'size' => 2 * 1024 * 1024,
+    ];
+    
+    expect(fn() => Upload::make()
+        ->maxSize(1) // 1 MB
+        ->folder('uploads')
+        ->upload($_FILES['arquivo']))
+        ->toThrow(Exception::class);
 });
